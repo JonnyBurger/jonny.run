d05e4bddbf9642dfb16d99e140b167e355088459
Jonny Burger <jonathanburger11@gmail.com>
2019-02-26 10:51:50 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import ordinal from 'ordinal';
+import {differenceInCalendarDays} from 'date-fns';
+
+const Container = styled.div`
+	text-align: center;
+`;
+
+const AppHeader = styled.div`
+	background-color: white;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	color: black;
+`;
+
+const Title = styled.div`
+	font-weight: 700;
+	font-family: 'Roboto Condensed';
+	font-size: 3em;
+`;
+
+const Counter = styled.div`
+	opacity: 0.4;
+	font-weight: bold;
+	font-family: 'Roboto Condensed';
+`;
+
+const dayOne = new Date('2016-02-19');
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				<AppHeader>
+					<Title>RUN EVERY DAY</Title>
+					<Counter>
+						Today is the{' '}
+						{ordinal(differenceInCalendarDays(new Date(), dayOne) + 1)} day
+					</Counter>
+				</AppHeader>
+			</Container>
+		);
+	}
 }
 
 export default App;
