7ec824167fc9c233cb3eb6ea5594f598f3008eb5
Jonny Burger <jonathanburger11@gmail.com>
2019-02-26 10:53:13 +0100

    Updates

---
+++ precache-manifest.54c0d7cd14fcc4230daec8c23810c8f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cca8e7ffa80ab710d292",
+    "url": "/static/js/main.cca8e7ff.chunk.js"
+  },
+  {
+    "revision": "a4bcc8c3faa4e7566fa8",
+    "url": "/static/js/2.a4bcc8c3.chunk.js"
+  },
+  {
+    "revision": "cca8e7ffa80ab710d292",
+    "url": "/static/css/main.787a83b3.chunk.css"
+  },
+  {
+    "revision": "8f0a70bf4390d940dcf9e108e37c7a2a",
+    "url": "/index.html"
+  }
+];
